b8d222347edfe161f8ac099c6b8b5d3a9a17438b
leejaeseong11 <easeon78@gmail.com>
2023-08-25 09:44:55 +0900

    Feat: create restaurant dto

--- src/restaurant/dto/RestaurantDTO.java
+++ src/restaurant/dto/RestaurantDTO.java
@@ -1,2 +1,19 @@
-package restaurant.dto;public class RestaurantDTO {
+package restaurant.dto;
+
+import category.dto.CategoryDTO;
+import menu.dto.MenuDTO;
+
+import java.util.ArrayList;
+
+public class RestaurantDTO {
+    private int id;
+    private String name;
+    private int viewCount;
+    private String runTime;
+    private String detailAddress;
+    private ArrayList<ReviewDTO> reviews;
+    private RegionDTO regions;
+    private ArrayList<CategoryDTO> category;
+    private ArrayList<MenuDTO> menu;
+
 }
