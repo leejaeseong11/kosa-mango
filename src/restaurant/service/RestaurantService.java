1760bb36eb0d0e3c9bcaa781eb71a1adcf806be4
leejaeseong11 <easeon78@gmail.com>
2023-08-28 16:17:48 +0900

    Docs: add comment restaurant service

--- src/restaurant/service/RestaurantService.java
+++ src/restaurant/service/RestaurantService.java
@@ -6,6 +6,7 @@ import restaurant.dto.RestaurantDTO;
 
 import java.util.ArrayList;
 
+
 public class RestaurantService {
     private int pageSize;
     private int[] restaurantIdList;
@@ -15,10 +16,20 @@ public class RestaurantService {
         restaurantIdList = new int[pageSize];
     }
 
+    /**
+     * @return 현재 검색한 결과의 전체 개수
+     */
     public int getRestaurantCount() {
         return allRestaurantCount;
     }
 
+    /**
+     * 유형에 따른 식당 리스트 출력 (식당 이름, 평점, 주소, 카테고리)
+     * @param searchType GENERAL_SEARCH-전체 검색, RANK_CATEGORY-카테고리 별 순위, RANK_REGION-지역 별 순위
+     * @param searchWord 전체 검색에서 사용할 키워드, 순위 출력의 경우 null
+     * @param index 1부터 시작하는 페이지
+     * @throws FindException
+     */
     public void printRestaurantList(String searchType, String searchWord, int index) throws FindException {
         RestaurantDAO rDao = new RestaurantDAO();
         ArrayList<RestaurantDTO> rDtoList = null;
@@ -54,10 +65,16 @@ public class RestaurantService {
         }
     }
 
+    /**
+     * 식당 하나의 상세 정보를 출력
+     * @param viewType VIEW_DETAIL-식당 상세 정보, VIEW_RANDOM-집 근처 랜덤 식당
+     * @param orderIndex 사용자가 선택한 식당 번호 (식당의 id = restaurantIdList[orderIndex-1])
+     * @throws FindException
+     */
     public void printDetailRestaurant(String viewType, int orderIndex) throws FindException {
         RestaurantDAO rDao = new RestaurantDAO();
         if (viewType.equals("VIEW_DETAIL")) {
-//        RestaurantDTO rDto = rDao.selectDetailRestaurantInfo(restaurantIdList[orderIndex-1]);
+
         } else if(viewType.equals("VIEW_RANDOM")) {
             RestaurantDTO rDto = rDao.randomRestaurantNearMyHouse(restaurantIdList[orderIndex-1]);
         }
