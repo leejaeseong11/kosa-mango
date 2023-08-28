013a4a98df79725ab7caa30f1ff9df8097e935d5
h0ci <gchy8899@gmail.com>
2023-08-28 14:22:28 +0900

    Feat : viewDetail and viewRank

--- src/restaurant/dao/RestaurantDAO.java
+++ src/restaurant/dao/RestaurantDAO.java
@@ -1,16 +1,17 @@
 package restaurant.dao;
 
-import category.dto.CategoryDTO;
-import exception.FindException;
-import jdbc.JDBC;
-import region.dto.RegionDTO;
-import restaurant.dto.RestaurantDTO;
-
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
+
+import category.dto.CategoryDTO;
+import exception.FindException;
+import jdbc.JDBC;
+import menu.dto.MenuDTO;
+import region.dto.RegionDTO;
+import restaurant.dto.RestaurantDTO;
 
 public class RestaurantDAO {
     private int restaurantCount;
@@ -130,4 +131,167 @@ public class RestaurantDAO {
 
         return returnedRestaurants;
     }
+    
+    public RestaurantDTO selectDetailRestaurantInfo(int restaurantId) throws FindException {
+        RestaurantDTO restaurantDetail = null;
+
+        Connection conn = null;
+        try {
+            conn = JDBC.connect();
+        } catch (Exception e) {
+            throw new FindException("DB 연결에 실패했습니다.");
+        }
+
+        PreparedStatement pstmt = null;
+        ResultSet rs = null;
+        try {
+            String sql = "SELECT res.restaurant_id, res.restaurant_name, NVL(res.rating_score, -1), res.view_count, res.run_time, reg.city_name, reg.si_gun_gu, reg.dong_eup_myeon, res.detail_address, c.category_name, m.menu_id, m.menu_name, m.price" +
+                    " FROM restaurants res" +
+                    " JOIN regions reg ON res.zipcode = reg.zipcode" +
+                    " JOIN restaurants_categories rc ON res.restaurant_id = rc.restaurant_id" +
+                    " JOIN categories c ON c.category_id = rc.category_id" +
+                    " JOIN menu m ON m.restaurant_id = res.restaurant_id" +
+                    " WHERE res.restaurant_id = ?";
+
+            pstmt = conn.prepareStatement(sql);
+            pstmt.setInt(1, restaurantId);
+            rs = pstmt.executeQuery();
+
+            if (rs.next()) {
+                restaurantDetail = new RestaurantDTO();
+                RegionDTO r = new RegionDTO();
+                restaurantDetail.setId(rs.getInt("restaurant_id"));
+                restaurantDetail.setName(rs.getString("restaurant_name"));
+                double ratingScore = rs.getDouble(3);
+                restaurantDetail.setRatingScore(Math.round(ratingScore * 100) / 100.0);
+                restaurantDetail.setViewCount(rs.getInt("view_count"));
+                restaurantDetail.setRunTime(rs.getString("run_time"));
+                restaurantDetail.setDetailAddress(rs.getString("detail_address"));
+                
+                r.setCityName(rs.getString("city_name"));
+                r.setSiGunGu(rs.getString("si_gun_gu"));
+                r.setDongEupMyeon(rs.getString("dong_eup_myeon"));
+                restaurantDetail.setRegion(r);
+                
+                CategoryDTO c = new CategoryDTO();
+                c.setName(rs.getString("category_name"));
+                ArrayList<CategoryDTO> categoriesList = new ArrayList<>();
+                categoriesList.add(c);
+                restaurantDetail.setCategories(categoriesList);
+                
+                MenuDTO m = new MenuDTO();
+                m.setId(rs.getInt("menu_id"));
+                m.setName(rs.getString("menu_name"));
+                m.setPrice(rs.getInt("price"));
+                ArrayList<MenuDTO> menuList = new ArrayList<>();
+                menuList.add(m);
+                restaurantDetail.setMenu(menuList);
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+            throw new FindException("식당 상세 정보 조회에 실패했습니다.");
+        } finally {
+            if (rs != null) {
+                try {
+                    rs.close();
+                } catch (SQLException e) {
+                    throw new FindException("DB 종료에 실패했습니다.");
+                }
+            }
+            if (pstmt != null) {
+                try {
+                    pstmt.close();
+                } catch (SQLException e) {
+                    throw new FindException("DB 종료에 실패했습니다.");
+                }
+            }
+            if (conn != null) {
+                try {
+                    conn.close();
+                } catch (SQLException e) {
+                    throw new FindException("DB 종료에 실패했습니다.");
+                }
+            }
+        }
+
+        return restaurantDetail;
+    }
+    
+    public ArrayList<RestaurantDTO> rankRestaurantsByCategory(String categoryId, int index) throws FindException {
+        ArrayList<RestaurantDTO> restaurantList = new ArrayList<>();
+
+        Connection conn = null;
+        try {
+            conn = JDBC.connect();
+        } catch (Exception e) {
+            throw new FindException("DB 연결에 실패했습니다.");
+        }
+
+        PreparedStatement pstmt = null;
+        ResultSet rs = null;
+        try {
+            String sql = "SELECT res.restaurant_id, res.restaurant_name, NVL(res.rating_score, -1), res.view_count, res.run_time, reg.city_name, reg.si_gun_gu, reg.dong_eup_myeon, res.detail_address" +
+                    " FROM restaurants res" +
+                    " JOIN regions reg ON res.zipcode = reg.zipcode" +
+                    " JOIN restaurants_categories rc ON res.restaurant_id = rc.restaurant_id" +
+                    " WHERE rc.category_id = ?" +
+                    " ORDER BY res.rating_score DESC, res.view_count DESC, res.restaurant_id" +
+                    " FETCH FIRST " + index + " ROWS ONLY";
+
+            pstmt = conn.prepareStatement(sql);
+            pstmt.setString(1, categoryId);
+            rs = pstmt.executeQuery();
+
+            while (rs.next()) {
+                RestaurantDTO restaurantDTO = new RestaurantDTO();
+                RegionDTO r = new RegionDTO();
+                restaurantDTO.setId(rs.getInt("restaurant_id"));
+                restaurantDTO.setName(rs.getString("restaurant_name"));
+                double ratingScore = rs.getDouble(3);
+                restaurantDTO.setRatingScore(Math.round(ratingScore * 100) / 100.0);
+                restaurantDTO.setViewCount(rs.getInt("view_count"));
+                restaurantDTO.setRunTime(rs.getString("run_time"));
+                restaurantDTO.setDetailAddress(rs.getString("detail_address"));
+                r.setCityName(rs.getString("city_name"));
+                r.setSiGunGu(rs.getString("si_gun_gu"));
+                r.setDongEupMyeon(rs.getString("dong_eup_myeon"));
+                restaurantDTO.setRegion(r);
+
+                restaurantList.add(restaurantDTO);
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+            throw new FindException("카테고리 내 식당 랭킹 조회에 실패했습니다.");
+        }  finally {
+            if (rs != null) {
+                try {
+                    rs.close();
+                } catch (SQLException e) {
+                    throw new FindException("DB 종료에 실패했습니다.");
+                }
+            }
+            if (pstmt != null) {
+                try {
+                    pstmt.close();
+                } catch (SQLException e) {
+                    throw new FindException("DB 종료에 실패했습니다.");
+                }
+            }
+            if (conn != null) {
+                try {
+                    conn.close();
+                } catch (SQLException e) {
+                    throw new FindException("DB 종료에 실패했습니다.");
+                }
+            }
+        }
+
+
+        return restaurantList;
+    }
+
+
+
 }
