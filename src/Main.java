1a4448f90f54a1e84430762da3c9d43ef592173e
leejaeseong11 <easeon78@gmail.com>
2023-08-29 18:24:53 +0900

    Feat: commit for merge

--- src/Main.java
+++ src/Main.java
@@ -3,24 +3,41 @@ import java.util.Scanner;
 
 public class Main {
     final static private int PAGE_SIZE = 5;
-    final static private int DIVIDE_REPEAT_COUNT = 100;
      static private String userId = "";
     public static void main(String[] args) {
         initService();
     }
+
+    /**
+     * 사용자가 선택할 메뉴 출력
+     * @param menu 메뉴 가변인자
+     */
     public static void printMenu(String ...menu) {
         for (int i = 0; i < menu.length; i++) {
             System.out.println(String.format("%d. %s", i+1, menu[i]));
         }
     }
+    /
+
+    /**
+     * 사용자가 선택한 메뉴 출력
+     * @param menu 인덱스가 포함된 메뉴 전체 이름
+     */
     public static void printDivide(String menu) {
         final int DIVIDE_REPEAT_COUNT = 100;
         if(menu == null) {
             System.out.println("=".repeat(DIVIDE_REPEAT_COUNT));
         } else {
-            int remainDivide = DIVIDE_REPEAT_COUNT - menu.length();
+            int remainDivide = DIVIDE_REPEAT_COUNT - menu.length() - 2;
+            System.out.print("=".repeat(remainDivide / 2));
+            System.out.print(String.format(" %s ", menu));
+            System.out.print("=".repeat(remainDivide / 2));
         }
     }
+
+    /**
+     * 서비스 실행 첫 화면
+     */
     public static void initService() {
         System.out.println("KOSA 플레이트에 방문해주셔서 감사합니다. 무엇을 도와드릴까요?");
         System.out.println("=".repeat(DIVIDE_REPEAT_COUNT));
@@ -28,7 +45,7 @@ public class Main {
 
         Scanner sc = new Scanner(System.in);
         if (userId.equals("")) {
-            while(!userChoice.equals("3")) {
+            while(!userChoice.equals("5")) {
                 printMenu("식당 검색하기", "식당 추천", "로그인", "회원 가입", "종료하기");
 
                 System.out.print("번호를 입력해주세요: ");
@@ -54,37 +71,42 @@ public class Main {
                         System.out.println("잘못된 입력입니다. 다시 입력해주세요.");
                 }
             }
-        } else {while(!userChoice.equals("3")) {
-            printMenu("식당 검색하기", "식당 추천", "내 정보 보기", "종료하기");
+        } else {
+            while(!userChoice.equals("4")) {
+                printMenu("식당 검색하기", "식당 추천", "내 정보 보기", "종료하기");
 
-            System.out.print("번호를 입력해주세요: ");
-            userChoice = sc.nextLine();
+                System.out.print("번호를 입력해주세요: ");
+                userChoice = sc.nextLine();
 
-            switch (userChoice) {
-                case "1":
-                    searchRestaurant();
-                    break;
-                case "2":
-                    recommendRestaurant();
-                    break;
-                case "3":
-                    viewMyInfo();
-                    break;
-                case "4":
-                    System.out.println("KOSA 플레이트를 종료합니다.");
-                    break;
-                default:
-                    System.out.println("잘못된 입력입니다. 다시 입력해주세요.");
+                switch (userChoice) {
+                    case "1":
+                        searchRestaurant();
+                        break;
+                    case "2":
+                        recommendRestaurant();
+                        break;
+                    case "3":
+                        viewMyInfo();
+                        break;
+                    case "4":
+                        System.out.println("KOSA 플레이트를 종료합니다.");
+                        break;
+                    default:
+                        System.out.println("잘못된 입력입니다. 다시 입력해주세요.");
+                }
             }
         }
-        }
 
-        System.out.println("=".repeat(DIVIDE_REPEAT_COUNT));
+        printDivide(null);
         System.out.println("감사합니다. 좋은 하루 보내세요^^");
         sc.close();
     }
+
+    /**
+     * 검색 화면 출력
+     */
     public static void searchRestaurant() {
-        System.out.println("=".repeat(DIVIDE_REPEAT_COUNT));
+        printDivide(null);
         Scanner sc = new Scanner(System.in);
         System.out.println("검색할 지역, 식당 또는 메뉴를 입력해주세요:");
 
