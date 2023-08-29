540acecdb36e2038506d33a9111c57a79b5147d9
leejaeseong11 <easeon78@gmail.com>
2023-08-29 16:16:44 +0900

    Fix: remove unnecessary field

--- src/review/dto/ReviewDTO.java
+++ src/review/dto/ReviewDTO.java
@@ -9,7 +9,6 @@ public class ReviewDTO {
 	private int id;
 	private String content;
 	private int rating;
-	private int totalRating;
 	private Date writingTime;
 	private int userId;
 	private int restaurantId;
